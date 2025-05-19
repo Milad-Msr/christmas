441ec6913b96c4c64bb0e79be8bde8adabe9499e
Milad Mansouri <Milad.Msrii@Gmail.com>
2025-05-19 15:57:31 +0330

    handle navbar in mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
-/*=============== SHOW MENU ===============*/
+// SHOW MENU 
+const navMenu = document.getElementById("nav-menu");
+const navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
+
+navToggle.addEventListener('click', () => {navMenu.classList.add('show-menu')});
+navClose.addEventListener('click', () => {navMenu.classList.remove('show-menu')});
 
 
-/*=============== REMOVE MENU MOBILE ===============*/
+
+// REMOVE MENU MOBILE 
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {navMenu.classList.remove('show-menu')});
+});
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
